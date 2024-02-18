e1768002ecea150fe70487e773d45ef9bf9c8842
1718wani <ikuya1293@gmail.com>
2024-02-18 21:21:49 +0900

    🎨 MantineUI Providerでラップ

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 
 const router = createRouter({ routeTree });
 
@@ -15,6 +17,8 @@ declare module "@tanstack/react-router" {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>
 );
