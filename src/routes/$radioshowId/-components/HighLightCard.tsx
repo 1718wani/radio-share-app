595d5b9369eb0c3cbb2436631aeae933a367c722
1718wani <ikuya1293@gmail.com>
2024-02-20 23:53:29 +0900

    💄 HighlightCardの装飾

--- src/routes/$radioshowId/-components/HighLightCard.tsx
+++ src/routes/$radioshowId/-components/HighLightCard.tsx
@@ -1,35 +1,74 @@
-import { Card, Text, Badge, Group, Accordion } from "@mantine/core";
+import {
+  Card,
+  Text,
+  Accordion,
+  Group,
+  ActionIcon,
+  Flex,
+  useMantineTheme,
+  Button,
+  Badge,
+} from "@mantine/core";
+import { IconBookmark, IconHeart } from "@tabler/icons-react";
 
-export const HighLightCard = () => {
+type props = {
+  title: string;
+  description: string;
+  playUrl: string;
+};
+
+export const HighLightCard = (props: props) => {
+  const { title, description, playUrl } = props;
+  const theme = useMantineTheme();
   return (
     <>
-      <Card withBorder padding="lg" radius="md" mx={"sm"}>
-        <Group justify="space-between">
-          <Badge>12 days left</Badge>
-        </Group>
+      <Card withBorder padding="md" radius="md" mx={"sm"}>
+        <Badge w="fit-content" variant="light">
+          NEW !
+        </Badge>
+        <Flex justify={"space-between"} mx={"sm"}>
+          <Text truncate fz="xl" fw={700} mt="sm">
+            {title}
+          </Text>
+          <Group gap={6}>
+            <ActionIcon variant="subtle" color="gray">
+              <IconHeart color={theme.colors.red[6]} />
+            </ActionIcon>
+            <ActionIcon variant="subtle" color="gray">
+              <IconBookmark color={theme.colors.yellow[6]} />
+            </ActionIcon>
+          </Group>
+        </Flex>
 
-        <Text fz="lg" fw={500} mt="md">
-          5.3 minor release (September 2022)
-        </Text>
         <Accordion>
           <Accordion.Item value="test">
             <Accordion.Control>説明</Accordion.Control>
             <Accordion.Panel>
               <Text fz="sm" c="dimmed" mt={5}>
-                Form context management, Switch, Grid and Indicator components
-                improvements, new hook and 10+ other changes
+                {description}
               </Text>
             </Accordion.Panel>
           </Accordion.Item>
         </Accordion>
 
-        <Text c="dimmed" fz="sm" mt="md">
-          Tasks completed:{" "}
-          <Text span fw={500} c="bright">
-            23/36
-          </Text>
-        </Text>
+        <Button
+          component="a" // ここで 'a' タグを指定
+          href={playUrl} // 遷移先のURLを指定
+          target="_blank"
+          variant="gradient"
+          gradient={{
+            from: "rgba(4, 201, 47, 1)",
+            to: "rgba(87, 70, 70, 1)",
+            deg: 158,
+          }}
+          mt={"sm"}
+          mx={"lg"}
+        >
+          Spotifyで再生する
+        </Button>
       </Card>
     </>
   );
 };
+
+// gradient={{ from: 'red', to: 'rgba(87, 70, 70, 1)', deg: 158 }}
--- src/routes/$radioshowId/-components/HighLightsList.tsx
+++ src/routes/$radioshowId/-components/HighLightsList.tsx
@@ -1,10 +1,60 @@
-import { Box } from "@mantine/core";
+import { Grid } from "@mantine/core";
 import { HighLightCard } from "./HighLightCard";
 
 export const HighLightsList = () => {
+  const CardData = [
+    {
+      title: "SpotifyOK",
+      description:
+        "ｊふぁいｊふぃおえわｊふぃおわｆじをｆじあおｆｊわいおｆじぇわいおｆじおｆｊわいおｆじぇをいｆｊわいおｆじゃいおｆｊ",
+      playUrl:
+        "https://open.spotify.com/episode/1aEZAwA3UHI6WqS8ktZFtF?si=i8MvZXESTJWwpFq6fh-uaQ&t=5&context=spotify%3Aepisode%3A1aEZAwA3UHI6WqS8ktZFtF",
+    },
+    {
+      title: "YoutubeOK",
+      description:
+        "ｊふぁいｊふぃおえわｊふぃおわｆじをｆじあおｆｊわいおｆじぇわいおｆじおｆｊわいおｆじぇをいｆｊわいおｆじゃいおｆｊ",
+      playUrl: "https://youtu.be/s5S9a3kaK7k?t=90",
+    },
+    {
+      title: "SpotifyOK",
+      description:
+        "ｊふぁいｊふぃおえわｊふぃおわｆじをｆじあおｆｊわいおｆじぇわいおｆじおｆｊわいおｆじぇをいｆｊわいおｆじゃいおｆｊ",
+      playUrl:
+        "https://open.spotify.com/episode/1aEZAwA3UHI6WqS8ktZFtF?si=i8MvZXESTJWwpFq6fh-uaQ&t=5&context=spotify%3Aepisode%3A1aEZAwA3UHI6WqS8ktZFtF",
+    },
+    {
+      title: "YoutubeOK",
+      description:
+        "ｊふぁいｊふぃおえわｊふぃおわｆじをｆじあおｆｊわいおｆじぇわいおｆじおｆｊわいおｆじぇをいｆｊわいおｆじゃいおｆｊ",
+      playUrl: "https://youtu.be/s5S9a3kaK7k?t=90",
+    },
+    {
+      title: "SpotifyOK",
+      description:
+        "ｊふぁいｊふぃおえわｊふぃおわｆじをｆじあおｆｊわいおｆじぇわいおｆじおｆｊわいおｆじぇをいｆｊわいおｆじゃいおｆｊ",
+      playUrl:
+        "https://open.spotify.com/episode/1aEZAwA3UHI6WqS8ktZFtF?si=i8MvZXESTJWwpFq6fh-uaQ&t=5&context=spotify%3Aepisode%3A1aEZAwA3UHI6WqS8ktZFtF",
+    },
+    {
+      title: "YoutubeOK",
+      description:
+        "ｊふぁいｊふぃおえわｊふぃおわｆじをｆじあおｆｊわいおｆじぇわいおｆじおｆｊわいおｆじぇをいｆｊわいおｆじゃいおｆｊ",
+      playUrl: "https://youtu.be/s5S9a3kaK7k?t=90",
+    },
+  ];
+
   return (
-    <Box mt={4}>
-      <HighLightCard />
-    </Box>
+    <Grid mt={10} mx={"sm"} justify="center">
+      {CardData.map((card) => (
+        <Grid.Col span={{ base: 11, md: 6, lg: 3 }}>
+          <HighLightCard
+            title={card.title}
+            description={card.description}
+            playUrl={card.playUrl}
+          />
+        </Grid.Col>
+      ))}
+    </Grid>
   );
 };
